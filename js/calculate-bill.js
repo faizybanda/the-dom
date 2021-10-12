1168e495009b830c9aec1ea2be08a3712fc50df5
faizybanda <faizy.banda@gmail.com>
2021-10-12 07:16:58 +0200

    done

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -7,10 +7,9 @@
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
-//link the function to a click event on the calculate button
+
 
 /*const billStringElement = document.querySelector(".billString");
 
--- js/settings-bill.js
+++ js/settings-bill.js
@@ -2,13 +2,12 @@
 
 // get refences to all the settings fields
 
-//get a reference to the add button
 
-//get a reference to the 'Update settings' button
+
 
 // create a variables that will keep track of all the settings
 
-// create a variables that will keep track of all three totals.
+
 
 //add an event listener for when the 'Update settings' button is pressed
 
